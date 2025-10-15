27dc4c44cd3cd488693d28075fc5e383c3773a4e
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2025-10-15 20:52:44 +0300

    modified importing issue

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import formidable from "express-formidable";
+import { isValidObjectId } from "mongoose";
 const router = express.Router();
 
 // --- Controllers ---
@@ -18,8 +19,19 @@ import {
 
 // --- Middlewares ---
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
-// This import is now correct because checkId.js uses 'export default'.
-import checkId from "../middlewares/checkId.js";
+// NOTE: The import for 'checkId' has been removed for this test.
+
+// --- INLINE MIDDLEWARE FOR DEBUGGING ---
+// The logic from checkId.js has been moved directly into this file.
+// This test will prove if the module import system is the source of the error.
+function checkId(req, res, next) {
+  if (!isValidObjectId(req.params.id)) {
+    res.status(404);
+    throw new Error(`Invalid Object ID: ${req.params.id}`);
+  }
+  next();
+}
+// --- END INLINE MIDDLEWARE ---
 
 router
   .route("/")
@@ -27,6 +39,8 @@ router
   .post(authenticate, authorizeAdmin, formidable(), addProduct);
 
 router.route("/allproducts").get(fetchAllProducts);
+
+// The 'checkId' function being used here is now the one defined locally in this file.
 router.route("/:id/reviews").post(authenticate, checkId, addProductReview);
 
 router.get("/top", fetchTopProducts);
